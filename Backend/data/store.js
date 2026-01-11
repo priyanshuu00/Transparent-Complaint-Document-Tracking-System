89134f71bdf6a6f077de003aead380428729576b
LuckySinghRawat <cyberboy3131@gmail.com>
2026-01-11 21:51:53 +0530

    data

---
+++ Backend/data/store.js
@@ -0,0 +1,14 @@
+module.exports = {
+  otps: {},
+  requests: [
+    {
+      id: "REQ-1001",
+      type: "FIR Copy Request",
+      status: "Completed",
+      department: "Police",
+      officer: "Officer Sharma",
+      timeTaken: "3 Days",
+      cost: "₹200"
+    }
+  ]
+};
